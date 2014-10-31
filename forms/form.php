c31010bee847bb7e2a6b4ee6d419a2db4df7b08c
ipotaje <ipotaje@gmail.com>
2014-10-31 22:41:29 +0000

    forms updated

--- forms/form.php
+++ forms/form.php
@@ -1,3 +1,29 @@
+<?php
+
+function searchForValue($campo, $array) {
+   // echo $campo;
+   // print_r($array);
+   foreach ($array as $val) {
+   		foreach ($val as $key => $obj){
+   			if ($obj === $campo){
+   				return $val['valor'];
+   			}
+   		}
+   }
+   
+   return null;
+}
+
+
+$url1 = 'http://localhost/map/require3/db/toJSON.php?cmd=configuracionesemail';
+$data = json_decode(file_get_contents($url1), true);
+
+$url2 = 'http://localhost/map/require3/db/toJSON.php?cmd=paginasform';
+$page =	json_decode(file_get_contents($url2), true);
+
+// print_r($data);
+// print_r(searchForValue('email_user', $data));
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,10 +36,11 @@
 		<div id="feedback">
 			<!-- Heading Of The Form -->
 			<div class="head">
+<!-- 			
 			<h3>Contacta</h3>
-				<p>Este es un formulario de contacto</p>
+			<p>Este es un formulario de contacto</p>
 			</div>
-			<!-- Feedback Form -->
+			
 			<form action="#" id="form" method="post" name="form">
 				<input name="vname" placeholder="Nombre:" type="text" value="">
 				<input name="vemail" placeholder="Correo electrónico:" type="text" value="">
@@ -21,10 +48,37 @@
 				<label>Comentario:</label>
 				<textarea name="msg" placeholder="Escribe tu comentario aquí..."></textarea>
 				<input id="send" name="submit" type="submit" value="Enviar">
-			</form>
+			</form> 
+			-->
+			<?php
+				// print_r($page);
+				echo $page[0]['content'];
+			?>
+<form action="#" id="form" method="post" name="form">
+
+	*Nombre:<br><input name="vname" placeholder="Nombre:" type="text" value="">
+
+	Teléfono:<br><input name="telefono" placeholder="Teléfono:" type="text" value="">
+
+	*Correo electrónico:<br><input name="vemail" placeholder="Correo electrónico:" type="text" value="">
+
+	Nombre del recurso:<input name="sub" placeholder="Título" type="text" value="">
+
+	Localización:Latitud:Longitud:
+
+	Fuente:<br>
+	<p>A continuación, puedes escribir la información que te gustaría incluir en Mapping Las Palmas de Gran Canaria. También puedes advertirnos de posibles errores y realizar comentarios o sugerencias. Gracias por tu colaboración.</p>
+
+	<label>Comentario:</label>
+	<textarea name="msg" placeholder="Escribe tu comentario aquí..."></textarea>
+
+	<input id="send" name="submit" type="submit" value="Enviar">
+</form>
+*Campos obligatorios
+
+Tus datos serán tratados de acuerdo a la L.O.P.D. 15/99 y no serán transmitidos a terceros sin tu consentimiento.			
 			<h3><?php include "secure_email_code.php";?></h3>
 		</div>
 	</div>
-	<!-- Feedback Form Ends Here -->
 </body>
 </html>
--- forms/secure_email_code.php
+++ forms/secure_email_code.php
@@ -4,7 +4,7 @@ require '../vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
 if(isset($_POST["submit"])){
 // Checking For Blank Fields..
 	if($_POST["vname"]==""||$_POST["vemail"]==""||$_POST["sub"]==""||$_POST["msg"]==""){
-		echo "Fill All Fields..";
+		echo searchForValue('email_incomplete', $data);
 	}else{
 // Check if the "Sender's Email" input field is filled out
 		$email=$_POST['vemail'];
@@ -13,7 +13,7 @@ if(isset($_POST["submit"])){
 // Validate E-mail Address
 		$email= filter_var($email, FILTER_VALIDATE_EMAIL);
 		if (!$email){
-			echo "Invalid Sender's Email";
+			echo searchForValue('email_incomplete', $data);
 		}
 		else{
 			// $subject = $_POST['sub'];
@@ -34,40 +34,30 @@ if(isset($_POST["submit"])){
 			// $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 			$mail->isSMTP();                                      // Set mailer to use SMTP
-			$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-			$mail->SMTPAuth = true;                               // Enable SMTP authentication
-			$mail->Username = 'palmtron@gmail.com';                 // SMTP username
-			$mail->Password = '193645';                           // SMTP password
-			$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-			$mail->Port = 465;                                    // TCP port to connect to
+			$mail->Host 		= searchForValue('email_smtphost', $data);  // Specify main and backup SMTP servers
+			$mail->SMTPAuth 	= searchForValue('email_SMTPAuth', $data);                               // Enable SMTP authentication
+			$mail->Username 	= searchForValue('email_user', $data);                 // SMTP username
+			$mail->Password 	= searchForValue('email_password', $data);                           // SMTP password
+			$mail->SMTPSecure 	= searchForValue('email_SMTPSecure', $data);                            // Enable TLS encryption, `ssl` also accepted
+			$mail->Port 		= searchForValue('email_port', $data);                                    // TCP port to connect to
 
 			$mail->From = $email;
 			$mail->FromName = $_POST["vname"];
-			$mail->addAddress('egomecor@gmail.com', 'Joe User');     // Add a recipient
-			// $mail->addAddress('ellen@example.com');               // Name is optional
-			// $mail->addReplyTo('info@example.com', 'Information');
-			// $mail->addCC('cc@example.com');
-			// $mail->addBCC('bcc@example.com');
-
-			// $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-			// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-			// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-			// $mail->isHTML(true);                                  // Set email format to HTML
+			$mail->addAddress('egomecor@gmail.com', 'Administrador Mapping LPA');     // Add a recipient
 
 			$mail->Subject = $_POST['sub'];
 			$mail->Body    = $_POST['msg'];
 			// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 			if(!$mail->send()) {
-			    echo 'No se pudo mandar el correo';
+			    echo searchForValue('email_error', $data);
 			    echo 'Error: ' . $mail->ErrorInfo;
 			} else {
-			    echo 'Se ha enviado el correo, gracias!!!';
+			    echo searchForValue('email_correcto', $data);
 			}
 		}
 	}
 }
-?>
 
 
 
